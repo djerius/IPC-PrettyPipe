e10b0ab8c3fad9f7d77fe59daa1a35cd2434cf77
dj@macabre.cfa.harvard.edu <dj@macabre.cfa.harvard.edu>
2006-09-20 16:37:04 -0400

    added license/copyright info

--- lib/PipeC/Cmd.pm
+++ lib/PipeC/Cmd.pm
@@ -1,3 +1,27 @@
+# --8<--8<--8<--8<--
+#
+# Copyright (C) 2006 Smithsonian Astrophysical Observatory
+#
+# This file is part of PipeC
+#
+# PipeC is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# PipeC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the 
+#       Free Software Foundation, Inc. 
+#       51 Franklin Street, Fifth Floor
+#       Boston, MA  02110-1301, USA
+#
+# -->8-->8-->8-->8--
+
 package PipeC::Cmd;
 
 use strict;
